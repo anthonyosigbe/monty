642b066e577d7f6230ec3674da44b0c0062bbb4e
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-19 09:18:01 -0700

    defined error used updated into monty.h file

--- monty.h
+++ monty.h
@@ -10,6 +10,23 @@
 
 
 
+#define ERR_BAD_INST	100
+#define ERR_BAD_MALL	101
+#define ERR_INVLD_PARM	102
+#define ERR_ARG_USG	200
+#define ERR_PUSH_USG	201
+#define ERR_PINT_USG	202
+#define ERR_POP_USG	203
+#define ERR_SWAP_USG	204
+#define ERR_ADD_USG	205
+#define ERR_SUB_USG	206
+#define ERR_DIV_USG	207
+#define ERR_DIV_ZRO	208
+#define ERR_MUL_USG	209
+#define ERR_MOD_USG	210
+#define ERR_PCH_USG	211
+#define ERR_PCH_EMP	212
+
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
