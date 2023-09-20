eb62575c26c4f4bc39d81149f5db3967dd727079
braveking12 <davidkadofficial@gmail.com>
2023-09-20 12:16:34 -0700

    Outputs the string beginning at the top of the stack

---
+++ print_string.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * pstr - Outputs the string beginning at the top of the stack,
+ * followed by a newline
+ * @stack: The stack's head
+ * @line_number: The line number where an error might occur
+ *
+ * Return: No return value
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void) line_number;
+
+	while (current && current->n != 0 && current->n >= 0 && current->n <= 127)
+	{
+		putchar(current->n);
+		current = current->next;
+	}
+
+	putchar('\n');
+}
