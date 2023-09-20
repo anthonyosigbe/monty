15be39e611dec36484b79709085ac0d8a85d3c7e
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-20 01:32:33 -0700

    3. swap tasks completed

---
+++ swap_function.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * swap - Interchanges the positions of the top two elements in the stack
+ * @stack: The stack's head
+ * @line_number: The line number where an error might occur
+ *
+ * Return: No return value
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	unsigned int length = count_stack(*stack);
+
+	if (length < 2)
+		error_mgt(ERR_SWAP_USG, NULL, line_number, NULL);
+	if (*stack)
+	{
+		int temp = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = temp;
+	}
+}
