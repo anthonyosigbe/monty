1c364cb6d81b0e694a3ac0002522ed95fe4f3069
braveking12 <davidkadofficial@gmail.com>
2023-09-19 21:39:53 -0700

    new prototype added for execution_mgt

--- monty.h
+++ monty.h
@@ -75,6 +75,7 @@ void stack_deallocation(void);
 void pint(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void (*func_src(char *s))(stack_t **, unsigned int);
+int execution_mgt(char *op_code, char *op_param, unsigned int line, int m);'
 
 
 #endif
