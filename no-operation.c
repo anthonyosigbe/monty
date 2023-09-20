e8a1421223f3ee5b0194b28766b9c59238567f05
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-20 01:41:02 -0700

    5. nop tasks completed

---
+++ no-operation.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - Performs no operation
+ * @stack: The stack's head
+ * @line_number: The line number where the error might occur
+ *
+ * Return: No return value
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
