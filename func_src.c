c3188bf6aeb8a247bc864eb78b7f4b6e152fb26e
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-19 17:17:23 -0700

    function source updated

---
+++ func_src.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * func_src - Choose the function corresponding to the Monty instruction
+ * @s: The instruction to be executed
+ *
+ * Return: A pointer to the function to be executed, or
+ * NULL if the function does not exist
+ */
+
+void (*func_src(char *s))(stack_t **, unsigned int)
+{
+	instruction_t insts[] = {
+		{ "push", push },
+		{ "push_queue", push_queue },
+		{ "pall", pall },
+		{ "pint", pint },
+		{ "pop", pop },
+		{ "swap", swap },
+		{ "add", add },
+		{ "nop", nop },
+		{ "sub", sub },
+		{ "div", divide },
+		{ "mul", mul },
+		{ "mod", mod },
+		{ "pchar", pchar },
+		{ "pstr", pstr },
+		{ "rotl", rotl },
+		{ "rotr", rotr },
+		{ NULL, NULL }
+	};
+	for (int i = 0; insts[i].opcode; i++)
+	{
+		if (strcmp(s, insts[i].opcode) == 0)
+			return (insts[i].f);
+	}
+	return (NULL);
+}
