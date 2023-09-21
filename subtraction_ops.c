ea7cc03e4b4ad925cf1bf7d682958e99229ed18d
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-21 02:30:35 -0700

    bug fix and update

--- subtraction_ops.c
+++ subtraction_ops.c
@@ -11,17 +11,16 @@
 
 void sub(stack_t **stack, unsigned int line_number)
 {
-	unsigned int length = stack_tracking(*stack);
 	stack_t *temp = *stack;
-	int a = temp->n;
-	int b = temp->next->n;
+	unsigned int a = 0, b = 0, length = 0;
+
+	length = stack_tracking(*stack);
 
 	if (length < 2)
 		error_mgt(ERR_SUB_USG, NULL, line_number, NULL);
 
 	a = temp->n;
 	b = temp->next->n;
-
 	temp->next->n = b - a;
 	*stack = temp->next;
 	free(temp);
