40ed6ce29774977730a471a14b3c5c393eb56808
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-20 21:51:47 -0700

    bug fix

--- division_ops.c
+++ division_ops.c
@@ -1,3 +1,5 @@
+#include "monty.h"
+
 /**
  * divide - Computes the result of dividing the second top element
  * by the top element of the stack
@@ -9,18 +11,24 @@
 
 void divide(stack_t **stack, unsigned int line_number)
 {
-	unsigned int length = count_stack(*stack);
-
-	if (length < 2)
-		error_mgt(ERR_DIV_USG, NULL, line_number, NULL);
-
+	unsigned int length = stack_tracking(*stack);
 	stack_t *temp = *stack;
 	int a = temp->n;
 	int b = temp->next->n;
 
-	if (a == 0)
-		error_mgt(ERR_DIV_ZRO, NULL, line_number, NULL);
+	if (length < 2)
+	{
+		error_mgt(ERR_DIV_USG, NULL, line_number, NULL);
+		return;
+	}
+	a = temp->n;
+	b = temp->next->n;
 
+	if (a == 0)
+	{
+		error_mgt(ERR_DIV_ZRO, NULL, line_number, NULL);
+		return;
+	}
 	temp->next->n = b / a;
 	*stack = temp->next;
 	free(temp);
